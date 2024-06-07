ef911beb524d5fd19e2945fdc3ac7c72720704fb
Penguin_Man <166058962+Harkirat-Jaswal@users.noreply.github.com>
2024-06-07 20:09:17 +0000

    k

--- Final/Hetties.js
+++ Final/Hetties.js
@@ -1,20 +1,16 @@
-
 const navMenu = document.getElementById('navMenu');
 
-
 navMenu.addEventListener('click', (event) => {
-
   if (event.target.tagName === 'A') {
-
-    const links = navMenu.getElementsByTagName('a');
-    for (let i = 0; i < links.length; i++) {
-      links[i].classList.remove('active');
-    }
-
-
+    const links = navMenu.querySelectorAll('a');
+    links.forEach(link => link.classList.remove('active'));
     event.target.classList.add('active');
   }
 });
+
+
+
+
 
 
 
